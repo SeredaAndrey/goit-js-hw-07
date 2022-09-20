db61aff043aa005299a4a0e1b05f98316b599497
Anrii Sereda <a.a.sereda@ukr.net>
2022-09-20 23:10:20 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divElementContainer = document.querySelector(".gallery");
+divElementContainer.insertAdjacentHTML("beforeend", createMarkup());
+
+function createMarkup() {
+  const markup = [];
+  for (const galleryItem of galleryItems) {
+    markup.push(
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+        <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}"
+         />
+        </a>
+        </div>`
+    );
+  }
+  return markup.join("");
+}
